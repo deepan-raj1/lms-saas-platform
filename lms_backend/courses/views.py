c2ada10aebd9445f4ef78aecd6b893b4159d55fd
Deepak <dhpnrj@gmail.com>
2026-04-29 12:48:33 +0530

    Added course creation API with instructor-based access control

--- lms_backend/courses/views.py
+++ lms_backend/courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Course
+from .serializers import CourseSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsInstructorOrAdmin
 
-# Create your views here.
+class CourseCreateView(generics.CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated, IsInstructorOrAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(instructor=self.request.user)
--- lms_backend/lms_backend/urls.py
+++ lms_backend/lms_backend/urls.py
@@ -22,6 +22,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/courses/', include('courses.urls')),
 
     # JWT Authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
